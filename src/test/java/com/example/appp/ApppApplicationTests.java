af77641d0de39af0df2bc08f1e9dc75019853764
dangeonMaster21 <dafae722@gmail.com>
2023-11-21 05:56:30 +0300

    initial commit

---
+++ src/test/java/com/example/appp/ApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
